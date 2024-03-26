eec29ac83a5d09f23d5e2bd9d9a04ec24f916ea5
erikknave <103737305+erikknave@users.noreply.github.com>
2024-03-26 14:06:18 +0100

    smaller changes to identify documentation

--- chatdev/chat_chain.py
+++ chatdev/chat_chain.py
@@ -1,3 +1,5 @@
+# Update documentation here
+
 import importlib
 import json
 import os
@@ -20,14 +22,16 @@ def check_bool(s):
 
 class ChatChain:
 
-    def __init__(self,
-                 config_path: str = None,
-                 config_phase_path: str = None,
-                 config_role_path: str = None,
-                 task_prompt: str = None,
-                 project_name: str = None,
-                 org_name: str = None,
-                 model_type: ModelType = ModelType.GPT_3_5_TURBO) -> None:
+    def __init__(
+        self,
+        config_path: str = None,
+        config_phase_path: str = None,
+        config_role_path: str = None,
+        task_prompt: str = None,
+        project_name: str = None,
+        org_name: str = None,
+        model_type: ModelType = ModelType.GPT_3_5_TURBO,
+    ) -> None:
         """
 
         Args:
@@ -47,11 +51,11 @@ class ChatChain:
         self.org_name = org_name
         self.model_type = model_type
 
-        with open(self.config_path, 'r', encoding="utf8") as file:
+        with open(self.config_path, "r", encoding="utf8") as file:
             self.config = json.load(file)
-        with open(self.config_phase_path, 'r', encoding="utf8") as file:
+        with open(self.config_phase_path, "r", encoding="utf8") as file:
             self.config_phase = json.load(file)
-        with open(self.config_role_path, 'r', encoding="utf8") as file:
+        with open(self.config_role_path, "r", encoding="utf8") as file:
 
             self.config_role = json.load(file)
 
@@ -63,10 +67,12 @@ class ChatChain:
         self.chat_turn_limit_default = 10
 
         # init ChatEnv
-        self.chat_env_config = ChatEnvConfig(clear_structure=check_bool(self.config["clear_structure"]),
-                                             brainstorming=check_bool(self.config["brainstorming"]),
-                                             gui_design=check_bool(self.config["gui_design"]),
-                                             git_management=check_bool(self.config["git_management"]))
+        self.chat_env_config = ChatEnvConfig(
+            clear_structure=check_bool(self.config["clear_structure"]),
+            brainstorming=check_bool(self.config["brainstorming"]),
+            gui_design=check_bool(self.config["gui_design"]),
+            git_management=check_bool(self.config["git_management"]),
+        )
         self.chat_env = ChatEnv(self.chat_env_config)
 
         # the user input prompt will be self-improved (if set "self_improve": "True" in ChatChainConfig.json)
@@ -90,20 +96,20 @@ class ChatChain:
         self.phase_module = importlib.import_module("chatdev.phase")
         self.phases = dict()
         for phase in self.config_phase:
-            assistant_role_name = self.config_phase[phase]['assistant_role_name']
-            user_role_name = self.config_phase[phase]['user_role_name']
-            phase_prompt = "\n\n".join(self.config_phase[phase]['phase_prompt'])
+            assistant_role_name = self.config_phase[phase]["assistant_role_name"]
+            user_role_name = self.config_phase[phase]["user_role_name"]
+            phase_prompt = "\n\n".join(self.config_phase[phase]["phase_prompt"])
             phase_class = getattr(self.phase_module, phase)
-            phase_instance = phase_class(assistant_role_name=assistant_role_name,
-                                         user_role_name=user_role_name,
-                                         phase_prompt=phase_prompt,
-                                         role_prompts=self.role_prompts,
-                                         phase_name=phase,
-                                         model_type=self.model_type,
-                                         log_filepath=self.log_filepath)
+            phase_instance = phase_class(
+                assistant_role_name=assistant_role_name,
+                user_role_name=user_role_name,
+                phase_prompt=phase_prompt,
+                role_prompts=self.role_prompts,
+                phase_name=phase,
+                model_type=self.model_type,
+                log_filepath=self.log_filepath,
+            )
             self.phases[phase] = phase_instance
-
-
 
     def make_recruitment(self):
         """
@@ -124,32 +130,44 @@ class ChatChain:
 
         """
 
-        phase = phase_item['phase']
-        phase_type = phase_item['phaseType']
+        phase = phase_item["phase"]
+        phase_type = phase_item["phaseType"]
         # For SimplePhase, just look it up from self.phases and conduct the "Phase.execute" method
         if phase_type == "SimplePhase":
-            max_turn_step = phase_item['max_turn_step']
-            need_reflect = check_bool(phase_item['need_reflect'])
+            max_turn_step = phase_item["max_turn_step"]
+            need_reflect = check_bool(phase_item["need_reflect"])
             if phase in self.phases:
-                self.chat_env = self.phases[phase].execute(self.chat_env,
-                                                           self.chat_turn_limit_default if max_turn_step <= 0 else max_turn_step,
-                                                           need_reflect)
+                self.chat_env = self.phases[phase].execute(
+                    self.chat_env,
+                    (
+                        self.chat_turn_limit_default
+                        if max_turn_step <= 0
+                        else max_turn_step
+                    ),
+                    need_reflect,
+                )
             else:
-                raise RuntimeError(f"Phase '{phase}' is not yet implemented in chatdev.phase")
+                raise RuntimeError(
+                    f"Phase '{phase}' is not yet implemented in chatdev.phase"
+                )
         # For ComposedPhase, we create instance here then conduct the "ComposedPhase.execute" method
         elif phase_type == "ComposedPhase":
-            cycle_num = phase_item['cycleNum']
-            composition = phase_item['Composition']
+            cycle_num = phase_item["cycleNum"]
+            composition = phase_item["Composition"]
             compose_phase_class = getattr(self.compose_phase_module, phase)
             if not compose_phase_class:
-                raise RuntimeError(f"Phase '{phase}' is not yet implemented in chatdev.compose_phase")
-            compose_phase_instance = compose_phase_class(phase_name=phase,
-                                                         cycle_num=cycle_num,
-                                                         composition=composition,
-                                                         config_phase=self.config_phase,
-                                                         config_role=self.config_role,
-                                                         model_type=self.model_type,
-                                                         log_filepath=self.log_filepath)
+                raise RuntimeError(
+                    f"Phase '{phase}' is not yet implemented in chatdev.compose_phase"
+                )
+            compose_phase_instance = compose_phase_class(
+                phase_name=phase,
+                cycle_num=cycle_num,
+                composition=composition,
+                config_phase=self.config_phase,
+                config_role=self.config_role,
+                model_type=self.model_type,
+                log_filepath=self.log_filepath,
+            )
             self.chat_env = compose_phase_instance.execute(self.chat_env)
         else:
             raise RuntimeError(f"PhaseType '{phase_type}' is not yet implemented.")
@@ -177,7 +195,10 @@ class ChatChain:
         root = os.path.dirname(filepath)
         # directory = root + "/WareHouse/"
         directory = os.path.join(root, "WareHouse")
-        log_filepath = os.path.join(directory, "{}.log".format("_".join([self.project_name, self.org_name,start_time])))
+        log_filepath = os.path.join(
+            directory,
+            "{}.log".format("_".join([self.project_name, self.org_name, start_time])),
+        )
         return start_time, log_filepath
 
     def pre_processing(self):
@@ -195,11 +216,17 @@ class ChatChain:
             for filename in os.listdir(directory):
                 file_path = os.path.join(directory, filename)
                 # logs with error trials are left in WareHouse/
-                if os.path.isfile(file_path) and not filename.endswith(".py") and not filename.endswith(".log"):
+                if (
+                    os.path.isfile(file_path)
+                    and not filename.endswith(".py")
+                    and not filename.endswith(".log")
+                ):
                     os.remove(file_path)
                     print("{} Removed.".format(file_path))
 
-        software_path = os.path.join(directory, "_".join([self.project_name, self.org_name, self.start_time]))
+        software_path = os.path.join(
+            directory, "_".join([self.project_name, self.org_name, self.start_time])
+        )
         self.chat_env.set_directory(software_path)
 
         # copy config files to software path
@@ -222,15 +249,19 @@ class ChatChain:
         preprocess_msg += "**task_prompt**: {}\n\n".format(self.task_prompt_raw)
         preprocess_msg += "**project_name**: {}\n\n".format(self.project_name)
         preprocess_msg += "**Log File**: {}\n\n".format(self.log_filepath)
-        preprocess_msg += "**ChatDevConfig**:\n {}\n\n".format(self.chat_env.config.__str__())
+        preprocess_msg += "**ChatDevConfig**:\n {}\n\n".format(
+            self.chat_env.config.__str__()
+        )
         preprocess_msg += "**ChatGPTConfig**:\n {}\n\n".format(chat_gpt_config)
         log_and_print_online(preprocess_msg)
 
         # init task prompt
-        if check_bool(self.config['self_improve']):
-            self.chat_env.env_dict['task_prompt'] = self.self_task_improve(self.task_prompt_raw)
+        if check_bool(self.config["self_improve"]):
+            self.chat_env.env_dict["task_prompt"] = self.self_task_improve(
+                self.task_prompt_raw
+            )
         else:
-            self.chat_env.env_dict['task_prompt'] = self.task_prompt_raw
+            self.chat_env.env_dict["task_prompt"] = self.task_prompt_raw
 
     def post_processing(self):
         """
@@ -252,13 +283,15 @@ class ChatChain:
         duration = (datetime2 - datetime1).total_seconds()
 
         post_info += "Software Info: {}".format(
-            get_info(self.chat_env.env_dict['directory'], self.log_filepath) + "\n\n🕑**duration**={:.2f}s\n\n".format(duration))
+            get_info(self.chat_env.env_dict["directory"], self.log_filepath)
+            + "\n\n🕑**duration**={:.2f}s\n\n".format(duration)
+        )
 
         post_info += "ChatDev Starts ({})".format(self.start_time) + "\n\n"
         post_info += "ChatDev Ends ({})".format(now_time) + "\n\n"
 
         if self.chat_env.config.clear_structure:
-            directory = self.chat_env.env_dict['directory']
+            directory = self.chat_env.env_dict["directory"]
             for filename in os.listdir(directory):
                 file_path = os.path.join(directory, filename)
                 if os.path.isdir(file_path) and file_path.endswith("__pycache__"):
@@ -270,9 +303,14 @@ class ChatChain:
         logging.shutdown()
         time.sleep(1)
 
-        shutil.move(self.log_filepath,
-                    os.path.join(root + "/WareHouse", "_".join([self.project_name, self.org_name, self.start_time]),
-                                 os.path.basename(self.log_filepath)))
+        shutil.move(
+            self.log_filepath,
+            os.path.join(
+                root + "/WareHouse",
+                "_".join([self.project_name, self.org_name, self.start_time]),
+                os.path.basename(self.log_filepath),
+            ),
+        )
 
     # @staticmethod
     def self_task_improve(self, task_prompt):
@@ -292,7 +330,8 @@ remember that the revised prompt should not contain more than 200 words,
 here is the short description:\"{}\". 
 If the revised prompt is revised_version_of_the_description, 
 then you should return a message in a format like \"<INFO> revised_version_of_the_description\", do not return messages in other formats.""".format(
-            task_prompt)
+            task_prompt
+        )
         role_play_session = RolePlaying(
             assistant_role_name="Prompt Engineer",
             assistant_role_prompt="You are an professional prompt engineer that can improve user input prompt to make LLM better understand these prompts.",
@@ -307,11 +346,19 @@ then you should return a message in a format like \"<INFO> revised_version_of_th
         # log_and_print_online("System", role_play_session.assistant_sys_msg)
         # log_and_print_online("System", role_play_session.user_sys_msg)
 
-        _, input_user_msg = role_play_session.init_chat(None, None, self_task_improve_prompt)
+        _, input_user_msg = role_play_session.init_chat(
+            None, None, self_task_improve_prompt
+        )
         assistant_response, user_response = role_play_session.step(input_user_msg, True)
-        revised_task_prompt = assistant_response.msg.content.split("<INFO>")[-1].lower().strip()
-        log_and_print_online(role_play_session.assistant_agent.role_name, assistant_response.msg.content)
+        revised_task_prompt = (
+            assistant_response.msg.content.split("<INFO>")[-1].lower().strip()
+        )
+        log_and_print_online(
+            role_play_session.assistant_agent.role_name, assistant_response.msg.content
+        )
         log_and_print_online(
             "**[Task Prompt Self Improvement]**\n**Original Task Prompt**: {}\n**Improved Task Prompt**: {}".format(
-                task_prompt, revised_task_prompt))
+                task_prompt, revised_task_prompt
+            )
+        )
         return revised_task_prompt
